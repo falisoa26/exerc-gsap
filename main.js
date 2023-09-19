46373c7aac6c12b6cc94173319d050fda71c9e18
falisoa26 <falisoan123@gmail.com>
2023-09-19 11:08:27 +0300

    commit1

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import gsap from "gsap";
 import './style.css';
-
+const text = "HELLO WORLD";
+const textArray = text.split('');
+const nav = document.querySelector('.navbar')
+for (let index = 0; index < textArray.length; index++) {
+  const span = document.createElement('span');
+  span.textContent = textArray[index];
+  nav.appendChild(span);
+}
 gsap.from('span', {
   opacity: 0,
   duration: 3,
